934c8b72af6ed6999bbbc8edf6bff49f692e6d6d
norouzy <mohamadnewday@gmail.com>
2023-07-03 19:33:08 +0330

    start

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,18 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "D:\\projects\\front\\animated-card\\app\\page.js": [
+      "D:\\projects\\front\\animated-card\\app\\page.module.css"
+    ],
+    "D:\\projects\\front\\animated-card\\app\\layout.js": [
+      "D:\\projects\\front\\animated-card\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\layout.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "D:\\projects\\front\\animated-card\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "D:\\projects\\front\\animated-card\\app\\page": [
+      "D:\\projects\\front\\animated-card\\app\\page.module.css",
+      "D:\\projects\\front\\animated-card\\app\\globals.css",
+      "D:\\projects\\front\\animated-card\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\layout.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
